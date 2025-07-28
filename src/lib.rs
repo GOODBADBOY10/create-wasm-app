9c7d807ceb55daa18be14f1466ced6aa7712052b
Goodbadboy <selfmanademola@gmail.com>
2025-07-28 01:02:02 +0100

    installed necessary dependecy

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+mod utils;
+
+use std::string;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+    // alert("Hello, {} wasm-game-of-life!");
+}
